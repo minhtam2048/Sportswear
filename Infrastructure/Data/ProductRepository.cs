bd02d0055a480868c07afb76d019558f6c2fc896
minhtam2048 <minhtam2048@gmail.com>
2021-03-05 22:20:13 +0700

    Returned category and brand when retrieved product(s)

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -22,17 +22,23 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<Category>> GetProductCategoriesAsync()
         {
-            return await _context.Categories.ToArrayAsync();
+            return await _context.Categories.ToListAsync();
         }
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products
+            .Include(p => p.Category)
+            .Include(p => p.Brand)
+            .FirstOrDefaultAsync(p => p.Id == id);
         }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+            .Include(p => p.Category)
+            .Include(p => p.Brand)
+            .ToListAsync();
         }
     }
 }
